fac9042e4302904c7abdf8a473a294326d66bfd8
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-04 20:40:39 -0500

    Delete Models directory

--- Models/MongoSettings.cs
+++
@@ -1,9 +0,0 @@
-
-namespace imagenes_ms
-{
-    public class MongoSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string Database { get; set; } = null!;
-    }
-}
