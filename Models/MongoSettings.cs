6fb463fa6fb8d6797f12d4d55703fe769eff138d
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-04 20:36:27 -0500

    Add files via upload

---
+++ Models/MongoSettings.cs
@@ -0,0 +1,9 @@
+
+namespace imagenes_ms
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string Database { get; set; } = null!;
+    }
+}
